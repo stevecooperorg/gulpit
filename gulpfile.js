f8332e0dce0c88dfa670eb939625e6de1317d62b
Steve Cooper <steve@stevecooper.org>
2017-01-20 22:15:20 +0000

    split the grunt file from the initialisation script

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,3 @@
-/*
- cd ~/IdeaProects/foo
- rm package.json
- rm -rf node_modules
- npm init -y
- npm install gulp --save-dev
- npm install jshint --save-dev
- npm install gulp-jshint --save-dev
- npm install gulp-watch --save-dev
- npm install jasmine --save-dev
- npm install gulp-jasmine --save-dev
- curl file:////Users/steve/IdeaProjects/foo/setup/gulpfile.js > gulpfile.js
- # curl https://gist.githubusercontent.com/stevecooperorg/c67898608d718c8c62c30dcfcee2100a/raw/gulpfile.js > gulpfile.js
- gulp watch
- node ./node_modules/gulp/bin/gulp.js watch
-
- */
 var src = ['index.js', "src/**.js"];
 var specs = ["spec/*.js"];
 
