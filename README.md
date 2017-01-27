ebed5dca921758feb642e7e7bf7721d8c1275ee7
Steve Cooper <steve@stevecooper.org>
2017-01-27 21:07:56 +0000

    stuff

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 To get running on mac or unix;
 
     curl -s https://raw.githubusercontent.com/stevecooperorg/gulpit/master/init | bash
+
+On windows/powershell
+
+    Invoke-WebRequest -uri https://raw.githubusercontent.com/stevecooperorg/gulpit/master/init -usebasicparsing | iex
--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,46 @@
 var src = ['index.js', "src/**.js"];
 var specs = ["spec/*.js"];
+var allJs = src.concat(specs);
+var allFiles = allJs.concat(['**/*.html', '**/*.css']);
 
-const gulp   = require('gulp');
-const jshint = require('gulp-jshint');
-const jasmine = require('gulp-jasmine');
-const watch  = require('gulp-watch');
+// DONE
+// lint code singly.
+// get jslint to work when the code changes.
 
-gulp.task('default', ['compile', 'test']);
+// TODO
+// get livereload server started
+// pipe changes to livereload -- js, html, css
+// get karma to work
 
-gulp.task('watch', function() {
-    gulp.watch(['spec/**', 'src/**'],   { ignoreInitial: false }, ['compile', 'test']);
-});
+const gulp       = require('gulp');
+const jshint     = require('gulp-jshint');
+const jasmine    = require('gulp-jasmine');
+const watch      = require('gulp-watch');
+const livereload = require('gulp-livereload');
 
-gulp.task("compile", function () {
+gulp.task('lint', function() {
+    // starts jshint
     gulp
-        .src(src)
-        .pipe(jshint({esversion: 5}))
+        .src(allJs)
+        .pipe(jshint({ esversion: 5 }))
         .pipe(jshint.reporter('default'));
 });
 
-gulp.task('test', function() {
+gulp.task('lr', function() {
+    // pipes all content (html, css, js, tests) through to started livereload server.
+    // <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
     gulp
-        .src(specs)
-        .pipe(jshint({esversion: 5}))
-        .pipe(jshint.reporter('default'))
-        .pipe(jasmine({
-            helpers: []
-        }));
+        .src(allFiles)
+        .pipe(livereload());
+});
+
+gulp.task('default', ['lint']);
+
+gulp.task('watch', function() {
+    // start a livereload server;
+    livereload.listen();
+    // when JS changes, rerun linting
+    gulp.watch(allJs, { ignoreInitial: false }, ['lint']);
+    // on any content file, kick off livereload
+    gulp.watch(allFiles, { ignoreInitial: false }, ['lr']);
 });
